f9dcf006e1b6bb217c50e264913e9ccdfd2f3c7b
Alexander Pinares Bolivar <alexander.bolivar@drtis.com.br>
2019-10-21 08:40:24 -0300

    base

---
+++ base.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define mod 1000000007
+#define ll long long
+#define ld long double
+#define ff first
+#define ss second
+#define endl '\n'
+#define pb push_back
+#define vi vector<int>
+#define f(i, y) for (int i = 0; i < y; i++)
+#define fx(i,x,y) for (int i = x; i < y; i++)
+#define fd(i,y) for(int i = y-1; i >= 0; i--)
+#define fdx(i,x,y) for(int i = y; i>= x; i--)
+#define FOR(it,A) for(typeof A.begin() it = A.begin(); it!=A.end(); it++)
+#define pii pair<int,int>
+#define sz(v) (int)v.size()
+#define watch(x) cerr<<(#x)<<"= "<<(x)<<endl;
+#define watcharr(a) cerr<<(#a)<<"("<<sz(a)<<")= ";rep(i,sz(a))cerr<<a[i]<<",";cerr<<'\n';
+#define FASTIO std::ios::sync_with_stdio(false),cin.tie(0),cout.tie(0);
+const int INF = 0x3f3f3f3f;
+const int MOD = (int)1e9 + 7;
+
+using namespace std;
+int main() {
+  FASTIO;
+  return 0;
+}
